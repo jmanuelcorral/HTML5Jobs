9e0861ff1c23a2f8c3faf7253b967c59caa04399
José Manuel Corral <jose.manuel.corral@gmail.com>
2013-03-08 10:33:15 +0100

    Solución al Segundo Ejercicio

--- js/common.js
+++ js/common.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	$("#btngeo").click(function(){
 		geoplugin.init();
+		$("#locationmap").fadeIn();
 	});
+
+	dragndrop.init("inputfile","dropzone","submitbutton");
+	geosearch.autocomplete;
 });
 
