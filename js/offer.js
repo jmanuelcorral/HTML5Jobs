f044b9df7ecee32d04cb6495a8a628bfce961117
José Manuel Corral <jose.manuel.corral@gmail.com>
2013-03-19 14:31:55 +0100

    Ejercicio 5
    ejercicio de hoy

--- js/offer.js
+++ js/offer.js
@@ -1,22 +1,28 @@
 var Offer = function(jobtitle, jobdescription, jobrequirements, jobsalary, incorporationdate) {
-//Propiedades de Offer
- return null;
+	this.title = jobtitle;
+	this.description = jobdescription;
+	this.requirement = jobrequirements;
+	this.salary = jobsalary;
+	this.incorporationdate = incorporationdate
 };
 //Creamos offerManager como un Namespace, el truco está hacerlo como un Literal Object vacío
 var offerManager = {};
 offerManager.traceTools = function (offer_info) {
 	return {
 		displayOfferInfoLog : function(){
-	
+			console.log("title:" + offer_info.title);
+			console.log("description:" + offer_info.description);
+			console.log("requirement:" + offer_info.requirement);
+			console.log("salary:" + offer_info.salary);
+			console.log("incorporationdate:" + offer_info.incorporationdate);
 		},
 		displayOfferInfoAlert : function(){
-			/*
 			var cadena = 	"title: " + offer_info.title + 
 							" description: " + offer_info.description +
 							" requirement: " + offer_info.requirement +
 							" salary: " + offer_info.salary + 
 							" incorporationdate: " + offer_info.incorporationdate;
-			*/
+
 			alert(cadena);
 		}
 	};
@@ -25,8 +31,12 @@ offerManager.traceTools = function (offer_info) {
 offerManager.Validation = function(offerdata) {
 	return {
 		fieldsAreNotNull : function(){
-			var isNotNull = false;
-			//Falta comprobar si los campos son o no son null
+			var isNotNull = true;
+			isNotNull = isNotNull && (offerdata.title != "");
+			isNotNull = isNotNull && (offerdata.description != "");
+			isNotNull = isNotNull && (offerdata.requirement != "");
+			isNotNull = isNotNull && (offerdata.salary != "");
+			isNotNull = isNotNull && (offerdata.incorporationdate != "");
 			return isNotNull;
 		}
 	}
@@ -34,14 +44,22 @@ offerManager.Validation = function(offerdata) {
 
 offerManager.Persistence = function(offer_info) {
 	var getFromForm = function(){
-		//llamada al constructor con los valores de la página
-		//devolver dicha llamada
+		return new Offer ($("#jobtitle").val(),
+						  $("#jobdescription").val(),
+						  $("#jobrequirements").val(),
+						  $("#jobsalary").val(),
+						  $("#incorporationdate").val());
 	}
 	var setInForm = function(){
-		//Set de los valores del objeto en el Formulario
+		$("#jobtitle").val(offer_info.title);
+		$("#jobdescription").val(offer_info.description);
+		$("#jobrequirements").val(offer_info.requirement);
+		$("#jobsalary").val(offer_info.requirement);
+		$("#incorporationdate").val(offer_info.incorporationdate);
 	}
 	var saveInServer = function(){
-		//De momento no hay que implementarlo, ¿próxima clase? ¿node?
+		var jsonSerializedValue = JSON.Stringify(offer_info);
+		alert(jsonSerializedValue);
 	}
 	return{
 		getter:getFromForm,
@@ -49,3 +67,26 @@ offerManager.Persistence = function(offer_info) {
 		save : saveInServer
 	}
 }
+
+offerManager.UI = function(){
+		$("#setdata").click(function(){
+			CURRENT_OFFER = offerManager.Persistence().getter();
+			offerManager.traceTools(CURRENT_OFFER).displayOfferInfoLog();
+		});
+
+		$("#getdata").click(function(){
+			if (CURRENT_OFFER != undefined){
+				offerManager.Persistence(CURRENT_OFFER).setter();
+			}
+		});
+
+		$("#removedata").click(function(){
+			$("#jobtitle").val("");
+			$("#jobdescription").val("");
+			$("#jobrequirements").val("");
+			$("#jobsalary").val("");
+			$("#incorporationdate").val("");
+		});
+};
+
+$(document).ready(offerManager.UI);
