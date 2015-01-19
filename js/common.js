4c9e56e2bce90dc348fa58260921d0ad12c104b6
José Manuel Corral <jose.manuel.corral@gmail.com>
2015-01-19 18:14:34 +0100

    tercera sesion
    Tercera sesion del curso

--- js/common.js
+++ js/common.js
@@ -1,23 +1,6 @@
-
-
 $(document).ready(function(){
-	//Variable global
-	var CURRENT_OFFER;
-
-	$("#submitbutton").click(function(){
-		var name = $("#cname").val();
-		var desc = $("#cdescription").val();
-		var mail = $("#cemail").val();
-		var phone = $("#cphone").val();
-		var companyinfo = new Company(name, desc, mail, phone);
-		var validation = company_validation(companyinfo);
-		var helper = company_helpers(companyinfo);
-		helper.displayCompanyInfoLog();
-		//helper.displayCompanyInfoAlert();
-		alert(validation.isValid());
+	$("#btngeo").click(function(){
+		geoplugin.init();
 	});
-	
-	ko.applyBindings(candidateManager.viewModel);
-
 });
 
